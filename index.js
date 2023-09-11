78f3bae7644501bb9c2e748513e55857f26950bc
Gunjan1116 <kumargunjan1116@gmail.com>
2023-09-11 11:59:30 +0530

    added now 2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -7,26 +7,20 @@ const postRoute = require('./routes/postRoute');
 const authRoute = require("./routes/authRoute");
 const likedPostsRoute = require("./routes/likedPostsRoute")
 const commentRoute = require("./routes/commentRoute")
-const profileRoute = require("./routes/profileRoute")
 const {connection}=require("./config/db")
+require('dotenv').config();
 
 const app = express();
-app.use(express.json());
-
- 
 // Enable CORS middleware
 app.use(cors());
 
+app.use(express.json());
 
-// Serve static images from the 'uploads' directory
-app.use("/uploads", express.static("uploads"));
+ 
 
-// mongoose connection
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   connectTimeoutMS: 30000, // Set a higher timeout value
-// });
+
+
+
 app.get("/",(req,res)=>{
   res.send("Welcome to Basic Home API");
 })
@@ -37,7 +31,6 @@ app.use('/post', postRoute);
 app.use("/auth", authRoute);
 app.use("/like", likedPostsRoute);
 app.use("/comment", commentRoute);
-app.use("/profile", profileRoute);
 
 app.listen(process.env.PORT, async () => {
   try {
