07ea74ce2aa147f532351f53f310c502ae4438cd
Gunjan1116 <kumargunjan1116@gmail.com>
2023-09-20 18:15:36 +0530

    added all backend code 1

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const {connection}=require("./config/db")
 const userRoute = require('./routes/userRoute');
 const postRoute = require('./routes/postRoute'); 
 const authRoute = require("./routes/authRoute");
